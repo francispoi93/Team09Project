ceb1e9d8dd71260d95ce396b40310f12f24bffa0
Gregory Billings <billingsgregory2@gmail.com>
2020-03-24 23:03:17 -0700

    Login form pre EF finish

--- ProjectTeam09/ProjectTeam09/LoginForm.cs
+++ ProjectTeam09/ProjectTeam09/LoginForm.cs
@@ -12,9 +12,49 @@ namespace ProjectTeam09
 {
     public partial class LoginForm : Form
     {
+        
         public LoginForm()
         {
             InitializeComponent();
+            buttonLoginSignIn.Click += (s, e) => login(textBoxLoginUsername.Text, textBoxLoginPassword.Text);
+        }
+        public void login(string inputID, string inputPass)
+        {
+            int userID = new int();
+            try {
+                userID = Int32.Parse(inputID);
+            } catch {
+                MessageBox.Show("your input for ID is in an improper format");
+            }
+            //bool usercheck = context.UserCredentials.UserID.contains(userID);
+            /*var databasePassword =
+                from n in context.UserCredentials
+                where n.UserID = userID
+                select n.Password;
+            bool passcheck = databasePassword == inputPass;
+            if (passcheck && usercheck) 
+            {
+                LoginInitialization(userID);
+            }
+            */
+        }
+        public void LoginInitialization(int userId)
+        {
+            AdminMainForm adminForm = new AdminMainForm();
+            StudentMainForm studentForm = new StudentMainForm();
+            ProfessorMainForm professorForm = new ProfessorMainForm();
+            if (999 < userId && userId < 1999)
+            {
+                studentForm.Show();
+            }
+            if (1999 < userId && userId < 2999) 
+            {
+                professorForm.Show();
+            }
+            if (2999 < userId && userId < 3999) 
+            {
+                adminForm.Show();
+            }
         }
     }
 }
