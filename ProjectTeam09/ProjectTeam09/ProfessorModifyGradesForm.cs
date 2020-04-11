323d5314702811fa66aaa8ab777c765cfb9ddf7e
Francis Poirier <poirierf@student.douglascollege.ca>
2020-04-10 21:13:21 -0700

    more professor logic

--- ProjectTeam09/ProjectTeam09/ProfessorModifyGradesForm.cs
+++ ProjectTeam09/ProjectTeam09/ProfessorModifyGradesForm.cs
@@ -20,91 +20,26 @@ namespace ProjectTeam09
             InitializeComponent();
             CourseID = courseid;
             InitializeForm();
-            buttonViewGrades.Click += ButtonViewGrades_Click;
-            buttonAddAssingment.Click += ButtonAddAssingment_Click;
+            buttonModifyGrades.Click += ButtonModifyGrades_Click;
         }
 
-        private void ButtonAddAssingment_Click(object sender, EventArgs e)
+        private void ButtonModifyGrades_Click(object sender, EventArgs e)
         {
-            listBoxAssignments.Items.Add(textBoxAddAssignment.Text);
+            context.SaveChanges();
+            MessageBox.Show("Changes saved.");
         }
-
-
-        private void ButtonViewGrades_Click(object sender, EventArgs e)
-        {
-
-            dataGridViewProfGrades.Rows.Clear();
-
-
-            var queryFilterAssignment = from filterGrade in context.Grades
-                                        where filterGrade.Assignment == listBoxAssignments.SelectedItem.ToString()
-                                        select filterGrade;
-
-
-            var queryDataGrid = from grades in context.Grades
-                                from student in context.Students
-                                where grades.CourseId == CourseID
-                                where grades.Assignment == listBoxAssignments.SelectedItem.ToString()
-                                select new
-                                {
-                                    Grade = grades.Grade1,
-                                    StudentID = student.StudentId,
-                                    StudentFirstName = student.FirstName,
-                                    StudentLastName = student.LastName,
-                                };
-       
-                foreach (var s in queryDataGrid)
-                {
-                    foreach (var h in queryFilterAssignment)
-                    {
-                        if (s.StudentID != h.StudentId)
-                        {
-                            dataGridViewProfGrades.Rows.Add(new string[] { s.StudentFirstName + " " + s.StudentLastName, "-" });
-
-                        }
-
-                        else
-                        {
-                            dataGridViewProfGrades.Rows.Add(new string[] { s.StudentFirstName + " " + s.StudentLastName, s.Grade.ToString() });
-
-                        }
-                    }
-                }
-            }
-
-        
 
         private void InitializeForm()
         {
             context.Students.Load();
             context.Courses.Load();
             context.Grades.Load();
-
-            var query = from grades in context.Grades
-                        where grades.CourseId == CourseID
-                        select new
-                        {
-                            Assignment = grades.Assignment
-                        };
-            foreach (var s in query)
-            {
-                if (listBoxAssignments.Items.Contains(s.Assignment))
-                {
-                }
-                else
-                {
-                    listBoxAssignments.Items.Add(s.Assignment);
-                }
-            }
             dataGridViewProfGrades.AllowUserToDeleteRows = false;
+            dataGridViewProfGrades.AllowUserToAddRows = true;
+            dataGridViewProfGrades.AllowUserToOrderColumns = true;
 
+            dataGridViewProfGrades.DataSource = context.Grades.Where(s => s.CourseId == CourseID).ToList();
 
-            DataGridViewTextBoxColumn[] columns = new DataGridViewTextBoxColumn[]
-            {
-                new DataGridViewTextBoxColumn() {Name = "Student Name"},
-                new DataGridViewTextBoxColumn() {Name = "Grade"},
-            };
-            dataGridViewProfGrades.Columns.AddRange(columns);
 
 
         }
